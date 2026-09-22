3186a9a8c3b1c7476847df3b3fd32641e4398856
André Paiva <andrefelipe13pp@gmail.com>
2026-09-22 14:37:50 -0300

    estudo de js

---
+++ Capitulo-01/variaveis.js
@@ -0,0 +1,12 @@
+let nome = "Homem-Aranha"
+let idade = 18
+let poder = "Inteligência"
+
+nome = "Flash"
+poder = "Velocidade"
+
+console.log(
+  `nome: ${nome}
+   Idade: ${idade}
+   Poder: ${poder}`
+)
